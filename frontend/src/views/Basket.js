bfe73c7cb8fe1aeab643b939a8f6bb48404e16fe
Никита <ng07031986@gmail.com>
2025-01-05 16:38:07 +0300

    init

---
+++ frontend/src/views/Basket.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Basket.css';
+import Product from '../components/Product';
+
+function Basket() {
+  return (
+    <div className="Basket">
+        <h1>Корзина</h1>
+    </div>
+  );
+}
+
+export default Basket;
